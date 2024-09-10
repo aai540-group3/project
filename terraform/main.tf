ffc2d67fd859fc06aadca9293617fb0d3f2598bf
jonathanagustin <5193877+jonathanagustin@users.noreply.github.com>
2024-09-10 07:58:16 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,7 +54,6 @@ resource "aws_s3_bucket_versioning" "enabled" {
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
   bucket = aws_s3_bucket.terraform_state.id
-
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -74,12 +73,10 @@ resource "aws_dynamodb_table" "terraform_locks" {
   name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
-
   attribute {
     name = "LockID"
     type = "S"
   }
-
   tags = {
     Name        = "Terraform State Locks"
     Environment = "Management"
