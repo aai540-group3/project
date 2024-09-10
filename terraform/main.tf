e4c593aba187b70ed62467d7bee923003b345cf7
jonathanagustin <5193877+jonathanagustin@users.noreply.github.com>
2024-09-10 07:56:09 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,12 +36,8 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.state_bucket_name
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
+  bucket        = var.state_bucket_name
+  force_destroy = true
   tags = {
     Name        = "Terraform State"
     Environment = "Management"
