0cc06259f16a7b1eae958e57903fedc716f3dd41
jonathanagustin <5193877+jonathanagustin@users.noreply.github.com>
2024-09-10 09:02:39 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,13 +20,13 @@ data "aws_organizations_organization" "org" {}
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.state_bucket_name
-  
+
   tags = {
     Name        = "Terraform State"
     Environment = "Management"
     ManagedBy   = "Terraform"
   }
-  
+
   lifecycle {
     prevent_destroy = true
     ignore_changes  = [bucket]
@@ -42,7 +42,7 @@ resource "aws_s3_bucket_versioning" "enabled" {
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
   bucket = aws_s3_bucket.terraform_state.id
-  
+
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -66,18 +66,18 @@ resource "aws_dynamodb_table" "terraform_locks" {
   name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
-  
+
   attribute {
     name = "LockID"
     type = "S"
   }
-  
+
   tags = {
     Name        = "Terraform State Locks"
     Environment = "Management"
     ManagedBy   = "Terraform"
   }
-  
+
   lifecycle {
     prevent_destroy = true
     ignore_changes  = [name]
