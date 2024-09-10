b1b3fc6e2d0693043aabc5821c408f254042e6ec
jonathanagustin <5193877+jonathanagustin@users.noreply.github.com>
2024-09-10 07:35:52 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,28 +27,13 @@ terraform {
       version = "~> 5.66.0"
     }
   }
-  backend "s3" {
-    bucket = var.state_bucket_name
-    key    = var.tf_state_key 
-    region = var.aws_region
-    dynamodb_table = var.dynamodb_table_name
-  }
 }
 
 provider "aws" {
   region = var.aws_region
 }
 
-data "aws_s3_bucket" "terraform_state" {
-  bucket = var.state_bucket_name
-}
-
-data "aws_dynamodb_table" "terraform_locks" {
-  name = var.dynamodb_table_name
-}
-
 resource "aws_s3_bucket" "terraform_state" {
-  count  = data.aws_s3_bucket.terraform_state.id == null ? 1 : 0
   bucket = var.state_bucket_name
 
   tags = {
@@ -63,16 +48,14 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
-  count  = data.aws_s3_bucket.terraform_state.id == null ? 1 : 0
-  bucket = aws_s3_bucket.terraform_state[0].id
+  bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-  count  = data.aws_s3_bucket.terraform_state.id == null ? 1 : 0
-  bucket = aws_s3_bucket.terraform_state[0].id
+  bucket = aws_s3_bucket.terraform_state.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -82,8 +65,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
 }
 
 resource "aws_s3_bucket_public_access_block" "terraform_state" {
-  count                   = data.aws_s3_bucket.terraform_state.id == null ? 1 : 0
-  bucket                  = aws_s3_bucket.terraform_state[0].id
+  bucket                  = aws_s3_bucket.terraform_state.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
@@ -91,7 +73,6 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  count        = data.aws_dynamodb_table.terraform_locks.id == null ? 1 : 0
   name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
@@ -103,7 +84,6 @@ resource "aws_dynamodb_table" "terraform_locks" {
 
   lifecycle {
     prevent_destroy = true
-    ignore_changes  = [name]
   }
 
   tags = {
