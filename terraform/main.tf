69d9094a5cfe7b2784c70e1912502e57a95cbec3
jonathanagustin <5193877+jonathanagustin@users.noreply.github.com>
2024-09-10 07:16:11 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,13 +112,3 @@ resource "aws_dynamodb_table" "terraform_locks" {
     ManagedBy   = "Terraform"
   }
 }
-
-output "s3_bucket_name" {
-  value       = data.aws_s3_bucket.terraform_state.id != null ? data.aws_s3_bucket.terraform_state.id : aws_s3_bucket.terraform_state[0].id
-  description = "The name of the S3 bucket"
-}
-
-output "dynamodb_table_name" {
-  value       = data.aws_dynamodb_table.terraform_locks.id != null ? data.aws_dynamodb_table.terraform_locks.id : aws_dynamodb_table.terraform_locks[0].id
-  description = "The name of the DynamoDB table"
-}
