88173fb917263939aadd1661b8ec8a57202bf3e7
jonathanagustin <5193877+jonathanagustin@users.noreply.github.com>
2024-09-10 06:26:13 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,17 +7,17 @@ data "aws_dynamodb_table" "terraform_locks" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
+  count  = data.aws_s3_bucket.terraform_state.id == null ? 1 : 0
   bucket = var.state_bucket_name
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes  = [bucket]
-  }
 
   tags = {
     Name        = "Terraform State"
     Environment = "Management"
     ManagedBy   = "Terraform"
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
 
@@ -47,6 +47,7 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
+  count        = data.aws_dynamodb_table.terraform_locks.id == null ? 1 : 0
   name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
